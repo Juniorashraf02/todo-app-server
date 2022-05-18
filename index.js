4294acdc964f8e117ee3e10491022e76a8a184bc
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-05-19 02:50:02 +0600

    project initialized and get api for tasks added

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.asxxn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const tasksCollections = await client.db('todo-app').collection('tasks');
+
+
+        app.get('/tasks', async (req, res) => {
+            const query = {};
+            const results = await tasksCollections.find(query).toArray();
+            res.send(results);
+        })
+
+        app.get('/', (req, res) => {
+            res.send('Hello World!')
+        })
+
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+
+    } finally { }
+}
+run().catch(console.dir);
+
+
+
+
